fba6266f38061392399631742b54490184d6ccc2
pmario26 <pmario26@gmail.com>
2017-02-13 18:21:32 -0200

    Add files via upload

--- TesteRep.py
+++ TesteRep.py
@@ -1,43 +1,32 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+nome = 'User_test'
+senha ='a'
 
-# Connects to the current device, returning a MonkeyDevice object
+#instalando app e preparando dispositivo
 device = MonkeyRunner.waitForConnection()
-
-# Installs the Android package. Notice that this method returns a boolean, so you can test
-# to see if the installation worked.
 device.installPackage('/home/pmario/AndroidStudioProjects/ConnectUFSCar-master/app/build/outputs/apk')
-
-# sets a variable with the package's internal name
 package = 'br.ufscar.connect'
-
-# sets a variable with the name of an Activity in the package
 activity = 'br.ufscar.connect.activities.LoginActivity'
-
-# sets the name of the component to start
 runComponent = package + '/' + activity
-
-# Runs the component
 device.startActivity(component=runComponent)
 
+#dorme para esperar animacao
 MonkeyRunner.sleep(2)
+#acende a tela
 device.wake()
-#device.touch(100,260,MonkeyDevice.DOWN_AND_UP)
+#seleciona a textbox e digita o username
 device.touch(100,200,MonkeyDevice.DOWN_AND_UP)
-device.type('User_test')
+device.type(nome)
 MonkeyRunner.sleep(1)
+#avanca para a proxima textbox e digita a senha
 device.touch(295,466,MonkeyDevice.DOWN_AND_UP)
 MonkeyRunner.sleep(1)
-#device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
-device.type('a')
-#MonkeyRunner.sleep(1)
-#device.touch(294,446,MonkeyDevice.DOWN_AND_UP)
+device.type(senha)
 MonkeyRunner.sleep(1)
+#fecha o teclado e clicka em "entrar"
 device.press("KEYCODE_BACK",MonkeyDevice.DOWN_AND_UP)
 MonkeyRunner.sleep(2)
 device.touch(171,332,MonkeyDevice.DOWN_AND_UP)
-#device.press("FLAG_EDITOR_ACTION", MonkeyDevice.DOWN_AND_UP)
-#aperta o ok denovo pra fazer login
-#device.press("KEYCODE_ENTER", MonkeyDevice.DOWN_AND_UP)
 
 
 #feed aberto
